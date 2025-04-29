0c98d3cbba227f5ce9417578ffcf660438580109
borgaveakshay <borgave.akshay@gmail.com>
2025-04-29 11:39:22 +0300

    - Employer Search screen changes

--- app/src/main/java/com/assignment/achmeaassignment/presentation/composables/EmployersList.kt
+++ app/src/main/java/com/assignment/achmeaassignment/presentation/composables/EmployersList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -14,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.assignment.achmeaassignment.domain.EmployerInfo
+import com.example.compose.AppTheme
 
 @Composable
 fun EmployersList(employersList: List<EmployerInfo>) {
@@ -21,11 +23,10 @@ fun EmployersList(employersList: List<EmployerInfo>) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(10.dp),
+            .wrapContentHeight(),
 
-    ) {
-        LazyColumn(){
+        ) {
+        LazyColumn() {
             items(items = employers.value, key = { item -> item.companyName }) {
                 EmployerInfoItem(it)
             }
@@ -36,37 +37,46 @@ fun EmployersList(employersList: List<EmployerInfo>) {
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun EmployersListNightPreview() {
-    EmployersList(
-        employersList = listOf(
-            EmployerInfo(
-                discountPercentage = 10,
-                companyName = "Company Name 1",
-                location = "Location 1",
-            ),
-            EmployerInfo(
-                discountPercentage = 11,
-                companyName = "Company Name 2",
-                location = "Location 2",
+    AppTheme {
+        Surface {
+            EmployersList(
+                employersList = listOf(
+                    EmployerInfo(
+                        discountPercentage = 10,
+                        companyName = "Company Name 1",
+                        location = "Location 1",
+                    ),
+                    EmployerInfo(
+                        discountPercentage = 11,
+                        companyName = "Company Name 2",
+                        location = "Location 2",
+                    )
+                )
             )
-        )
-    )
+        }
+    }
+
 }
 
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
 @Composable
 fun EmployersListPreview() {
-    EmployersList(
-        employersList = listOf(
-            EmployerInfo(
-                discountPercentage = 10,
-                companyName = "Company Name 1",
-                location = "Location 1",
-            ),
-            EmployerInfo(
-                discountPercentage = 11,
-                companyName = "Company Name 2",
-                location = "Location 2",
+    AppTheme {
+        Surface {
+            EmployersList(
+                employersList = listOf(
+                    EmployerInfo(
+                        discountPercentage = 10,
+                        companyName = "Company Name 1",
+                        location = "Location 1",
+                    ),
+                    EmployerInfo(
+                        discountPercentage = 11,
+                        companyName = "Company Name 2",
+                        location = "Location 2",
+                    )
+                )
             )
-        )
-    )
+        }
+    }
 }
--- app/src/main/java/com/assignment/achmeaassignment/presentation/employersearch/ui/EmployerSearchScreen.kt
+++ app/src/main/java/com/assignment/achmeaassignment/presentation/employersearch/ui/EmployerSearchScreen.kt
@@ -1,8 +1,127 @@
 package com.assignment.achmeaassignment.presentation.employersearch.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.assignment.achmeaassignment.domain.EmployerInfo
+import com.assignment.achmeaassignment.presentation.composables.EmployerSearchField
+import com.assignment.achmeaassignment.presentation.composables.EmployersList
+import com.assignment.achmeaassignment.presentation.employersearch.states.EmployerSearchState
+import com.example.compose.AppTheme
 
 @Composable
-fun EmployerSearchScreen() {
+fun EmployerSearchScreen(
+    onSearchQueryChanged: (String) -> Unit,
+    employerSearchState: EmployerSearchState
+) {
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp, top = 20.dp)
+    ) {
+        EmployerSearchField { searchQuery ->
+            onSearchQueryChanged(searchQuery)
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Column {
+            employerSearchState.apply {
+                when {
+                    !errorMessage.isNullOrEmpty() && isError -> {
+                        Text(
+                            text = errorMessage,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.error,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                    }
+
+                    employerSearchState.isLoading -> {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .size(50.dp)
+                        )
+                    }
+
+                    else -> {
+                        data?.let { employers ->
+                            EmployersList(employersList = employers)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun EmployerSearchScreenNightPreview() {
+    AppTheme {
+        Surface {
+            EmployerSearchScreen(
+                onSearchQueryChanged = {},
+                employerSearchState = EmployerSearchState(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = null,
+                    data = listOf(
+                        EmployerInfo(
+                            discountPercentage = 10,
+                            companyName = "company name 1",
+                            location = "location 1"
+                        ),
+                        EmployerInfo(
+                            discountPercentage = 20,
+                            companyName = "company name 2",
+                            location = "location 2"
+                        )
+                    )
+                )
+            )
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun EmployerSearchScreenPreview() {
+    AppTheme {
+        Surface {
+            EmployerSearchScreen(
+                onSearchQueryChanged = {},
+                employerSearchState = EmployerSearchState(
+                    isLoading = false,
+                    isError = false,
+                    errorMessage = null,
+                    data = listOf(
+                        EmployerInfo(
+                            discountPercentage = 10,
+                            companyName = "company name 1",
+                            location = "location 1"
+                        ),
+                        EmployerInfo(
+                            discountPercentage = 20,
+                            companyName = "company name 2",
+                            location = "location 2"
+                        )
+                    )
+                )
+            )
+        }
+    }
 }
